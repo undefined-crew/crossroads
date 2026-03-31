9bbdefeba4e3ecdcc554df1ff90358e5c23b3b76
Shaurya Chauhan <blues9524@gmail.com>
2026-03-31 20:17:47 +0530

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next"
+import { Geist, Geist_Mono } from "next/font/google"
+import "@/styles/main/globals.css"
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"]
+})
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"]
+})
+
+export const metadata: Metadata = {
+  title: "Crossroads - The Life Sandbox"
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body>{children}</body>
+    </html>
+  )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import styles from "@/styles/main/home.module.css"
+
+export default function Home() {
+  return (
+    <div className={styles.main}>
+      <h1>Home</h1>
+    </div>
+  )
+}
